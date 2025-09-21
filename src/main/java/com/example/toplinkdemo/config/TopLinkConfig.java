7d76c4e945a10d33e33069340eda11734bfe38e5
Takesver Kumar Thakur <tkthakur101@gmail.com>
2025-09-21 20:37:15 +0530

    Initial commit

--- src/main/java/com/example/toplinkdemo/config/TopLinkConfig.java
+++ src/main/java/com/example/toplinkdemo/config/TopLinkConfig.java
@@ -1,6 +1,5 @@
 package com.example.toplinkdemo.config;
 
-import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.context.annotation.Bean;
@@ -22,24 +21,6 @@ public class TopLinkConfig {
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setDataSource(dataSource);
-        emf.setPackagesToScan("com.example.toplinkdemo.entity");
-        // emf.setPersistenceUnitName("toplinkPU"); // Removed to avoid persistence.xml interference
-
-        // Set EclipseLink provider class (must be on classpath)
-        try {
-            Class<?> provider = Class.forName("org.eclipse.persistence.jpa.PersistenceProvider");
-            emf.setPersistenceProviderClass((Class<? extends PersistenceProvider>) provider);
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException("EclipseLink provider not found on classpath. Ensure the EclipseLink dependency is present.", e);
-        }
-
-        Properties props = new Properties();
-        props.put("eclipselink.logging.level", "FINE");
-        props.put("eclipselink.ddl-generation", "none");
-        props.put("eclipselink.target-database", "org.eclipse.persistence.platform.database.H2Platform");
-        props.put("eclipselink.weaving", "false");
-        emf.setJpaProperties(props);
-
         return emf;
     }
 
